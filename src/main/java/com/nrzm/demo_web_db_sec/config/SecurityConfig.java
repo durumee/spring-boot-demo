06b01f4c21ec4ee19fdbb73faa35707ed3d466f3
durumee <durumee@gmail.com>
2024-05-24 17:30:42 +0900

    JWT 인증

--- src/main/java/com/nrzm/demo_web_db_sec/config/SecurityConfig.java
+++ src/main/java/com/nrzm/demo_web_db_sec/config/SecurityConfig.java
@@ -56,18 +56,9 @@ public class SecurityConfig {
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화 (폼로그인으로 대체)
-                .formLogin(form -> form
-                        .loginPage("/loginPage")    // /login 설정시 문제발생, 회피할것
-                        .loginProcessingUrl("/perform_login")   //미설정시 POST /login 요청을 처리해야 하나 문제발생, 다음처럼 설정할것
-                        .defaultSuccessUrl("/", true)   //로그인 성공 시 이동 경로
-                        .permitAll()
-                )
-
-                //로그아웃은 /logout URL 이 기본값이며 동작함
-                .logout(logout -> logout
-                        .permitAll()
-                )
+                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화 (JWT로그인으로 대체)
+                .formLogin(form -> form.disable())  //formLogin 비활성화 (JWT 로그인으로 대체)
+                .logout(logout -> logout.disable()) // logout 비활성화 (JWT 토큰 만료로 대체)
                 .exceptionHandling(exception -> exception
                         .accessDeniedHandler(accessDeniedHandler())
                 )
