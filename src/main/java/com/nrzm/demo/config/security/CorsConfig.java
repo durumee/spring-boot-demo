def9314f22835c99388f5e5e7e573edb507b5cc8
durumee <durumee@gmail.com>
2024-06-19 11:43:14 +0900

    도커 및 cors 설정 추가

--- src/main/java/com/nrzm/demo/config/security/CorsConfig.java
+++ src/main/java/com/nrzm/demo/config/security/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8080")); // 허용할 출처를 명시적으로 지정
-//        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 출처 허용 패턴 지정
+        // configuration.setAllowedOrigins(Arrays.asList("http://localhost")); // 허용할 출처를 명시적으로 지정
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 출처 허용 패턴 지정
         configuration.setAllowedMethods(Arrays.asList("*")); // 모든 HTTP 메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
